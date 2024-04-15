ef30dfbac1325f301ac1e60fc543004407e16932
David Peñalver <david13penalver@gmail.com>
2024-04-15 19:12:30 +0200

    Updated cart and cartDetail Entity

--- src/main/java/com/dimentials/shop/domain/entity/Cart.java
+++ src/main/java/com/dimentials/shop/domain/entity/Cart.java
@@ -1,11 +1,31 @@
 package com.dimentials.shop.domain.entity;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
-    private Integer cartId;
-    private Integer clientId;
-    //private List<CartItem> items;
-    //private Date creationDate;
-    //private Date closingDate;
+    private Integer id;
+    private Client client;
+    private List<CartDetail> cartDetailList;
+    private BigDecimal total;
+
+    public Cart(Integer id) {
+        this.id = id;
+    }
+
+    public void addCartDetail(CartDetail cartDetail) {
+        if (cartDetailList == null) {
+            cartDetailList = new ArrayList<>();
+        }
+        cartDetailList.add(cartDetail);
+        total = total.add(cartDetail.getTotal());
+    }
+
+    public List<CartDetail> getCartDetailList() {
+        if (cartDetailList == null) {
+            cartDetailList = new ArrayList<>();
+        }
+        return cartDetailList;
+    }
 }
--- src/main/java/com/dimentials/shop/domain/entity/CartDetail.java
+++ src/main/java/com/dimentials/shop/domain/entity/CartDetail.java
@@ -1,6 +1,58 @@
 package com.dimentials.shop.domain.entity;
 
+import java.math.BigDecimal;
+
 public class CartDetail {
-    
-    
+    private Integer id;
+    private Card card;
+    private Integer quantity;
+    private BigDecimal price;
+
+    public BigDecimal getTotal() {
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+
+    public CartDetail(Card card, Integer quantity) {
+        this.card = card;
+        this.quantity = quantity;
+    }
+
+    public CartDetail(Integer id, Card card, Integer quantity, BigDecimal price) {
+        this.id = id;
+        this.card = card;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
