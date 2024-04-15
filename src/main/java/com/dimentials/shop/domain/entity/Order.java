b8a8eddad3c57ec01e35628fae3673e0a841c264
David Peñalver <david13penalver@gmail.com>
2024-04-15 19:15:45 +0200

    Updated order and orderDetail Entity

--- src/main/java/com/dimentials/shop/domain/entity/Order.java
+++ src/main/java/com/dimentials/shop/domain/entity/Order.java
@@ -1,5 +1,26 @@
 package com.dimentials.shop.domain.entity;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    private Integer id;
+    private Client client;
+    private List<OrderDetail> orderDetailList;
+    private BigDecimal total = BigDecimal.ZERO;
+
+    public Order(Integer id) {
+        this.id = id;
+    }
+
+    public void addOrderDetail(OrderDetail orderDetail) {
+        if(this.orderDetailList == null) {
+            this.orderDetailList = new ArrayList<>();
+        }
+        this.orderDetailList.add(orderDetail);
+        this.total = this.total.add(orderDetail.getTotal());
+    }
 
 }
--- src/main/java/com/dimentials/shop/domain/entity/OrderDetail.java
+++ src/main/java/com/dimentials/shop/domain/entity/OrderDetail.java
@@ -1,5 +1,22 @@
 package com.dimentials.shop.domain.entity;
 
+import java.math.BigDecimal;
+
 public class OrderDetail {
+
+    private Integer id;
+    private Card card;
+    private Integer quantity;
+    private BigDecimal price;
+
+    public BigDecimal getTotal() {
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+
+    public OrderDetail(Integer id, Integer quantity, BigDecimal price) {
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+    }
     
 }
