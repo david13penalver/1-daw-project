9802481e8db4b2567cd8f1a3402e23da89f35bab
David Peñalver <david13penalver@gmail.com>
2024-04-15 18:09:06 +0200

    Updated Card Entity

--- src/main/java/com/dimentials/shop/domain/entity/Card.java
+++ src/main/java/com/dimentials/shop/domain/entity/Card.java
@@ -1,47 +1,27 @@
 package com.dimentials.shop.domain.entity;
 
+import java.math.BigDecimal;
+
 public class Card {
 
     private Integer id;
     private String name;
     private String description;
+    private BigDecimal price;
+
+    public Card(Integer id, String name, String description, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Card() {
+    }
 
     public Card(Integer id, String name, String description) {
         this.id = id;
         this.name = name;
         this.description = description;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Card{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
     }
 }
