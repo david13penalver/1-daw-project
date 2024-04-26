139797fa9499c0988ed3766aabfb36006088a8c4
David Peñalver <david13penalver@gmail.com>
2024-04-26 08:49:06 +0200

    Fixed the database structure

--- documentacion/dimentials.sql
+++ documentacion/dimentials.sql
@@ -4,7 +4,7 @@ USE dimentials;
 
 -- User table
 CREATE TABLE user (
-    id_client INT PRIMARY KEY,
+    id_user INT PRIMARY KEY,
     name_user VARCHAR(50) not null,
     surname1 VARCHAR(25) not null,
     surname2 VARCHAR(25),
@@ -24,7 +24,7 @@ CREATE TABLE card (
 
 -- Monsters table
 CREATE TABLE monster (
-    id_card INT PRIMARY KEY, -- PREGUNTAR SI AL ESTAR ABAJO HAY QUE ELIMINARLA
+    id_card INT PRIMARY KEY, 
     atack INT not null,
     life INT not null,
     main_type_es  VARCHAR(20) not null,
@@ -36,7 +36,7 @@ CREATE TABLE monster (
 
 -- Spell table
 CREATE TABLE spell (
-    id_card INT PRIMARY KEY, -- PREGUNTAR SI AL ESTAR ABAJO HAY QUE ELIMINARLA
+    id_card INT PRIMARY KEY, 
     mana INT not null,
     FOREIGN KEY (id_card) REFERENCES card(id_card) ON DELETE CASCADE
 );
@@ -44,21 +44,21 @@ CREATE TABLE spell (
 -- Order table
 CREATE TABLE order_global (
     id_order_global INT PRIMARY KEY,
-    id_client INT,
+    id_user INT,
     status boolean not null,
     date_creation DATETIME not null,
     date_close DATETIME not null,
-    total DECIMAL(10, 2) not null,
-    FOREIGN KEY (id_client) REFERENCES client(id_client) ON DELETE CASCADE
-); -- NO CREA POR LA REFERENCIA A LA TABLA CLIENTE
+    total DECIMAL(10, 2) not null, -- TIENE QUE HACER LA SUMA DE LOS PRICE DE ORDER_DETAIL
+    FOREIGN KEY (id_user) REFERENCES user(id_user) ON DELETE CASCADE
+); 
 
 -- Order_details table
 CREATE TABLE order_detail (
     id_order_detail INT PRIMARY KEY,
     id_order_global INT,
     id_card INT,
-    price DECIMAL(10, 2) not null, -- ES EL MISMO QUE EN CARD, HABRÍA QUE COGERLO DE AHÍ
-    quantity INT not null, -- TIENE QUE SER UN CAMPO QUE MULTIPLICA PRICE * QUANTITY
+    price DECIMAL(10, 2) not null, -- TIENE QUE SER UN CAMPO QUE MULTIPLICA PRICE * QUANTITY
+    quantity INT not null,
     FOREIGN KEY (id_order_global) REFERENCES order_global(id_order_global) ON DELETE CASCADE,
     FOREIGN KEY (id_card) REFERENCES card(id_card) ON DELETE CASCADE
 );
