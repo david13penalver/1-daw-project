0e7ae6666d551ec146d8161c8827ad5386efcf0a
Borjover <borjover@gmail.com>
2024-05-02 16:34:15 +0200

    Nuevo Navbar implementado

---
+++ src/main/resources/static/js/menu_nav_dimential.js
@@ -0,0 +1,5 @@
+function toggleSidebar() {
+    document.querySelector(".sidebar").classList.toggle("d-none");
+    document.querySelector(".sidebar").classList.toggle("d-md-block");
+    document.querySelector(".overlay").classList.toggle("d-md-none");
+}
