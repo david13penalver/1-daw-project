48616ceaf2fe53a2811b9d0c6bab961fee83fb5b
Toni Suarez <144905621+lueyo31@users.noreply.github.com>
2024-05-10 09:55:57 +0200

    added all tests

--- src/test/java/com/dimentials/shop/mock/repository/CardDaoMock.java
+++ src/test/java/com/dimentials/shop/mock/repository/CardDaoMock.java
@@ -33,30 +33,70 @@ public class CardDaoMock implements CardDao {
         return null;
     }
 
-
     @Override
     public MonsterEntity addMonster(MonsterEntity monsterEntity) {
 
-        return null;
+        cardEntityList.add(monsterEntity);
+        return monsterEntity;
     }
 
     @Override
     public SpellEntity addSpell(SpellEntity spellEntity) {
-        return null;
+        cardEntityList.add(spellEntity);
+        return spellEntity;
     }
 
     @Override
     public CardEntity deleteCard(Integer id) {
-        return null;
+        CardEntity card1 = null;
+        for (CardEntity card : cardEntityList) {
+            if (card.getId().equals(id)) {
+                card1 = card;
+            }
+        }
+        if (card1 != null) {
+            cardEntityList.remove(card1);
+        }
+        return card1;
     }
 
     @Override
     public SpellEntity updateSpell(SpellEntity spellEntity) {
-        return null;
+
+        for (CardEntity cardEntity : cardEntityList) {
+            if (cardEntity.getId().equals(spellEntity.getId())) {
+
+                cardEntity.setId(spellEntity.getId());
+                cardEntity.setName(spellEntity.getName());
+                cardEntity.setDescription(spellEntity.getDescription());
+                cardEntity.setPrice(spellEntity.getPrice());
+                cardEntity.setImgPath(spellEntity.getImgPath());
+                spellEntity.setMana(spellEntity.getMana());
+
+            } else {
+                throw new IllegalArgumentException("The spell does not exist");
+            }
+            return spellEntity;
+        }
     }
 
     @Override
     public MonsterEntity updateMonster(MonsterEntity monsterEntity) {
-        return null;
+        for (CardEntity cardEntity : cardEntityList) {
+            if (cardEntity.getId().equals(monsterEntity.getId())) {
+                cardEntity.setId(monsterEntity.getId());
+                cardEntity.setName(monsterEntity.getName());
+                cardEntity.setDescription(monsterEntity.getDescription());
+                cardEntity.setPrice(monsterEntity.getPrice());
+                cardEntity.setImgPath(monsterEntity.getImgPath());
+                monsterEntity.setAttack(monsterEntity.getAttack());
+                monsterEntity.setLife(monsterEntity.getLife());
+                monsterEntity.setMainType(monsterEntity.getMainType());
+                monsterEntity.setSecondaryType(monsterEntity.getSecondaryType());
+            } else {
+                throw new IllegalArgumentException("The monster does not exist");
+            }
+            return monsterEntity;
+        }
     }
 }
--- src/test/java/com/dimentials/shop/unit/persistence/repository/CardRepositoryImplTest.java
+++ src/test/java/com/dimentials/shop/unit/persistence/repository/CardRepositoryImplTest.java
@@ -1,37 +1,37 @@
 package com.dimentials.shop.unit.persistence.repository;
 
-import com.dimentials.shop.domain.entity.Card;
-import com.dimentials.shop.mock.repository.CardDaoMock;
-import com.dimentials.shop.persistence.dao.CardDao;
-import com.dimentials.shop.persistence.repository.CardRepository;
-import com.dimentials.shop.persistence.repository.impl.CardRepositoryImpl;
-import com.dimentials.shop.persistence.repository.mapper.CardMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.*;
+
+import com.dimentials.shop.domain.entity.Card;
+import com.dimentials.shop.domain.entity.Monster;
+import com.dimentials.shop.domain.entity.Spell;
+import com.dimentials.shop.mock.repository.CardDaoMock;
+import com.dimentials.shop.persistence.dao.CardDao;
+import com.dimentials.shop.persistence.repository.CardRepository;
+import com.dimentials.shop.persistence.repository.impl.CardRepositoryImpl;
+import com.dimentials.shop.persistence.repository.mapper.CardMapper;
 
 public class CardRepositoryImplTest {
     private final CardDao cardDao = new CardDaoMock();
     private final CardRepository cardRepository = new CardRepositoryImpl(cardDao);
 
+
     private List<Card> expectedList;
+
     @BeforeEach
     void setUp() {
         expectedList = new ArrayList<>(
                 List.of(
                         new Card(1, "Nebulsmokar", "Monstruo", new BigDecimal(1), "Nebulsmokar.jpg"),
                         new Card(2, "Lizzar", "Monstruito", new BigDecimal(1), "Lizzar.jpg"),
-                        new Card(3, "Frogan", "Monstruitillo", new BigDecimal(1), "Frogan.jpg")
-                )
-        );
+                        new Card(3, "Frogan", "Monstruitillo", new BigDecimal(1), "Frogan.jpg")));
     }
 
     @Test
@@ -78,6 +78,99 @@ public class CardRepositoryImplTest {
             Card card3 = CardMapper.toCard(cardDao.findById(10));
 
             assertNull(card3);
+        }
+    }
+    @Nested
+    @DisplayName("AddingTests")
+    class AddingTests {
+        @Test
+        @DisplayName("The methods addMonster() returns the monster inserted")
+        void addMonsterReturnsTheMonsterInserted() {
+            Monster monster = new Monster(4, "cuerk", "Monstruo despiadado", new BigDecimal(7), "binchilling.jpg", 6, 7,
+                    "Fuego", "Agua");
+
+            Monster monsterInserted = CardMapper.toMonster(cardDao.addMonster(CardMapper.toMonsterEntity(monster)));
+
+            assertEquals(monster, monsterInserted);
+        }
+
+        @Test
+        @DisplayName("The methods addSpell() returns the spell inserted")
+        void addSpellReturnsTheSpellInserted() {
+            Spell spell = new Spell(4, "dario", "mala locura", new BigDecimal(13), "Dario.jpg", 9);
+
+            Spell spellInserted = CardMapper.toSpell(cardDao.addSpell(CardMapper.toSpellEntity(spell)));
+
+            assertEquals(spell, spellInserted);
+        }
+    }
+ 
+
+
+    @Nested
+    @DisplayName("UpdateTests")
+    class UpdateTests {
+        @Test
+        @DisplayName("The methods updateSpell() returns the spell updated")
+        void updateSpellReturnsTheSpellUpdated() {
+            Spell spell = new Spell(4, "dario", "mala locura", new BigDecimal(13), "Dario.jpg", 9);
+
+            Spell spellUpdated = CardMapper.toSpell(cardDao.updateSpell(CardMapper.toSpellEntity(spell)));
+
+            assertEquals(spell, spellUpdated);
+        }
+        @Test
+        @DisplayName("The methods updateMonster() returns the monster updated")
+        void updateMonsterReturnsTheMonsterUpdated() {
+            Monster monster = new Monster(1, "cuerk", "Monstruo despiadado", new BigDecimal(7), "binchilling.jpg", 6, 7,
+                    "Fuego", "Agua");
+
+            Monster monsterUpdated = CardMapper.toMonster(cardDao.updateMonster(CardMapper.toMonsterEntity(monster)));
+
+            assertEquals(monster, monsterUpdated);
+        }
+
+        @Test
+        @DisplayName("The methods updateMonster() but with ID not found")
+        void updateMonsterButWithIdNotFound() {
+            Monster monster = new Monster(10, "cuerk", "Monstruo despiadado", new BigDecimal(7), "binchilling.jpg", 6, 7,
+                    "Fuego", "Agua");
+
+            Monster monsterUpdated = CardMapper.toMonster(cardDao.updateMonster(CardMapper.toMonsterEntity(monster)));
+
+            assertThrows(IllegalArgumentException.class, () -> monsterUpdated);
+        }
+        @Test
+        @DisplayName("The methods updateSpell() but with ID not found")
+        void updateSpellButWithIdNotFound() {
+            Spell spell = new Spell(10, "dario", "mala locura", new BigDecimal(13), "Dario.jpg", 9);
+
+            Spell spellUpdated = CardMapper.toSpell(cardDao.updateSpell(CardMapper.toSpellEntity(spell)));
+
+            assertThrows(IllegalArgumentException.class, () -> spellUpdated);
+        }
+    }   
+    @Nested
+    @DisplayName("DeleteTests")
+    class DeleteTests {
+        @Test
+        @DisplayName("The methods deleteCard() returns the card deleted")
+        void deleteCardReturnsTheCardDeleted() {
+            Card card = new Card(1, "Nebulsmokar", "Monstruo", new BigDecimal(1), "Nebulsmokar.jpg");
+
+            Card cardDeleted = CardMapper.toCard(cardDao.deleteCard(1));
+
+            assertEquals(card, cardDeleted);
+        }
+
+        @Test
+        @DisplayName("The methods deleteCard() but with ID not found")
+        void deleteCardButWithIdNotFound() {
+            Card card = new Card(10, "Nebulsmokar", "Monstruo", new BigDecimal(1), "Nebulsmokar.jpg");
+
+            Card cardDeleted = CardMapper.toCard(cardDao.deleteCard(10));
+
+            assertThrows(IllegalArgumentException.class, () -> cardDeleted);
         }
     }
 }
