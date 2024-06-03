26260805b5b38b83329c346fd30a8fe9cfd5199e
David Peñalver <david13penalver@gmail.com>
2024-06-03 09:46:03 +0200

    Added the functionality to select the language of the fields of the database according to the locale language

--- src/main/java/com/dimentials/shop/persistence/dao/impl/jdbc/CardDaoJdbc.java
+++ src/main/java/com/dimentials/shop/persistence/dao/impl/jdbc/CardDaoJdbc.java
@@ -32,7 +32,7 @@ public class CardDaoJdbc implements CardDao {
         Locale currentLocale = LocaleContextHolder.getLocale();
         String language = currentLocale.getLanguage();
         try {
-            ResultSet resultSet = RawSql.select("SELECT * FROM card WHERE id_card = ?", List.of(id));
+            ResultSet resultSet = RawSql.select("SELECT id_card, name_card, description_" + language + ", price, image FROM card WHERE id_card = ?", List.of(id));
             if (!resultSet.next()) {
                 return null;
             }
