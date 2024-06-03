2be2dc41925af99f96300d3cf5cecab1553defa6
David Peñalver <david13penalver@gmail.com>
2024-06-03 12:58:51 +0200

    Final structure of H2 tests created

--- src/test/java/com/dimentials/shop/unit/persistence/dao/CardDaoJdbcTest.java
+++ src/test/java/com/dimentials/shop/unit/persistence/dao/CardDaoJdbcTest.java
@@ -7,10 +7,7 @@ import com.dimentials.shop.persistence.dao.entity.MonsterEntity;
 import com.dimentials.shop.persistence.dao.entity.SpellEntity;
 import com.dimentials.shop.persistence.dao.impl.jdbc.CardDaoJdbc;
 import com.dimentials.shop.persistence.dao.impl.jdbc.rawSql.DatabaseConnection;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
@@ -38,16 +35,72 @@ public class CardDaoJdbcTest {
         connection.getConnection().rollback();
     }
 
-    @Test
-    void findAll_shouldReturnAllCards() {
-        List<CardEntity> result = cardDao.findAll();
-        assertEquals(expectedLlibreList, result);
+    @Nested
+    class findTests {
+        @Test
+        void findAll_shouldReturnAllCards() {
+            List<CardEntity> result = cardDao.findAll();
+            assertEquals(expectedLlibreList, result);
+        }
+
+        @Test
+        void findById_shouldReturnCard() {
+            CardEntity result = cardDao.findById(1);
+            assertEquals(expectedLlibreList.get(0), result);
+        }
     }
 
-    @Test
-    void findById_shouldReturnCard() {
-        CardEntity result = cardDao.findById(1);
-        assertEquals(expectedLlibreList.get(0), result);
+    @Nested
+    class InsertionTests {
+        @Test
+        @Disabled
+        void addMonster_shouldInsertMonster() {
+            MonsterEntity monster = new MonsterEntity(null, "Name", "Description_es.", "Description_en", new BigDecimal(7), "image", 1, 1, "mainType_es", "mainType_en", "secondaryType_es", "secondaryType_en");
+            cardDao.addMonster(monster);
+            expectedLlibreList.add(monster);
+            CardEntity result = cardDao.findById(3);
+            assertEquals(expectedLlibreList.get(2), result);
+        }
+
+        @Test
+        @Disabled
+        void addSpell_shouldInsertSpell() {
+            SpellEntity spell = new SpellEntity(3, "Lavacus", "Escudo (1): Genera 1 de armadura al final de cada uno de tus turnos. La armadura se suma a la vida y se consume cuando tu legión dimensional es atacada. La armadura de esta habilidad es acumulable y la que no es consumida permanece incluso cuando la legión evoluciona.", new BigDecimal(7), "image", 1);
+            cardDao.addSpell(spell);
+            List<CardEntity> result = cardDao.findAll();
+            assertEquals(expectedLlibreList.size() + 1, result.size());
+        }
     }
+
+    @Nested
+    class alterationTests {
+        @Test
+        @Disabled
+        void updateMonster_shouldUpdateMonster() {
+            MonsterEntity monster = new MonsterEntity(1, "Name", "Description_es.", "Description_en", new BigDecimal(7), "image", 1, 1, "mainType_es", "mainType_en", "secondaryType_es", "secondaryType_en");
+            cardDao.updateMonster(monster);
+            CardEntity result = cardDao.findById(1);
+            assertEquals(monster, result);
+        }
+
+        @Test
+        @Disabled
+        void updateSpell_shouldUpdateSpell() {
+            SpellEntity spell = new SpellEntity(1, "Lavacus", "Escudo (1): Genera 1 de armadura al final de cada uno de tus turnos. La armadura se suma a la vida y se consume cuando tu legión dimensional es atacada. La armadura de esta habilidad es acumulable y la que no es consumida permanece incluso cuando la legión evoluciona.", new BigDecimal(7), "image", 1);
+            cardDao.updateSpell(spell);
+            CardEntity result = cardDao.findById(1);
+            assertEquals(spell, result);
+        }
+    }
+    @Nested
+    class DeletionTests {
+        @Test
+        void deleteCard_shouldDeleteCard() {
+            cardDao.deleteCard(1);
+            List<CardEntity> result = cardDao.findAll();
+            assertEquals(expectedLlibreList.size() - 1, result.size());
+        }
+    }
+
 
 }
